2afaf45676643160a8cb521d24186b815a1fc066
TomaszLaski <tomasz.laski0001@gmail.com>
2021-04-01 14:53:18 +0200

    Navbar Comleted

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-custom">
+            <div class="container">
+                <a className="navbar-brand logo" href="#">
+                    Tomasz Łaski's Portfolio
+				</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <FontAwesomeIcon icon={faBars} style={{color:"#eff9f2"}}/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">
+                                Home <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                About Me
+							</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Portfolio
+							</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Contact
+							</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

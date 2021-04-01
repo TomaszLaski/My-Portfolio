604a0f60c207c1160ebf9a7e48a22f2c1c0290e6
TomaszLaski <tomasz.laski0001@gmail.com>
2021-04-01 22:27:54 +0200

    Hero Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Particles from "react-particles-js";
+import Hero from "./components/Hero";
 
 function App() {
-  return (
-    <Navbar />
-  );
+	return (
+		<>
+			<Particles
+				params={{
+					particles: {
+						color: {
+							value: ["#03674f"],
+						},
+						line_linked: {
+							color: "#78a1bb",
+							opacity: 1,
+						},
+						number: {
+							value: 25,
+							density: {
+								enable: true,
+								value_area: 900,
+							},
+						},
+						shape: {
+							type: "circle",
+							stroke: {
+								width: 6,
+								color: "#2D62F7",
+							},
+						},
+					},
+				}}
+			/>
+			<Navbar />
+			<Hero />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Typed from "react-typed";
+import logo from "../laptop-img.svg";
+
+const Hero = () => {
+	return (
+		<div className="hero-wraper">
+			<div className="main-info">
+				<div class="text-container">
+					<h1>
+						The Fastets Learning <br /> Junior Web Developer
+					</h1>
+					<Typed
+						className="typed-text"
+						strings={["JavaScript", "React", "Html", "CSS"]}
+						typeSpeed={50}
+						backSpeed={70}
+						loop
+					/>
+				</div>
+				<img className="hero-logo" src={logo} alt="logo" />
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
